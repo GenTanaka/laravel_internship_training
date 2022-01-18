4fce826166564f92f0703db0dd8a0a8f04658bfc
gentanaka <gen.tanaka2001@gmail.com>
2022-01-18 14:26:39 +0900

    create MigrationFile & DBTable
    マイグレーションファイル作成
    テーブル作成

---
+++ database/migrations/2022_01_18_051810_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('body');
+            $table->string('image');
+            $table->UnsignedBigInteger('category_id');
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
